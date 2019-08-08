d5cc446655ad007331f109cd9a04d36009ddddfa
김용욱/TVUX혁신스쿼드 <wooki5813@sk.com>
2019-08-08 18:33:51 +0900

    [19.08.08]-아이템포커싱기능 추가

--- src/CarouselTimeline.js
+++ src/CarouselTimeline.js
@@ -5,10 +5,11 @@ class CarouselTimeline extends React.Component {
   constructor(props){
     super(props);
     this.tlDiv = React.createRef();
-    this.contDiv = React.createRef();
+    this.itemDiv =[]; //React.createRef();
 
     this.state = {
       contents : this.props.data
+      ,idx : 0
     }
     this.mouseState = {
       isClick : false
@@ -16,16 +17,29 @@ class CarouselTimeline extends React.Component {
       ,nowX : 0
       ,currentX : 0
     }
+    this.slideState = {
+      currentX : 0
+      ,idx : 0
+      ,maxSlide : this.state.contents.length
+      ,focusingColor : "rgba(253, 0, 0, 0.7);"
+    }
 
     this.handleMouseDown = this.handleMouseDown.bind(this);
     this.handleMouseMove = this.handleMouseMove.bind(this);
     this.handleMouseUp = this.handleMouseUp.bind(this);
     this.handleMouseOut = this.handleMouseOut.bind(this);
+
+    this.handleOnLeftBtn = this.handleOnLeftBtn.bind(this);
+    this.handleOnRightBtn = this.handleOnRightBtn.bind(this);
   }
 
   componentDidMount() {
+    console.log("componentDidMount");
     
+    this.itemDiv[0].childNodes[0].setAttribute("style","border:3px solid "+this.slideState.focusingColor);
+    this.itemDiv[0].childNodes[1].setAttribute("style","border:3px solid "+this.slideState.focusingColor);
   }
+
 
   handleMouseDown(e) {
     console.log("handleMouseDown"+e.clientX);
@@ -48,46 +62,82 @@ class CarouselTimeline extends React.Component {
   handleMouseOut(e){
     console.log("handleMouseOut");
     this.mouseState.isClick = false;
+    
   }
 
-  moveTimeline(_x){
-    this.tlDiv.current.style.transform="translateX("+_x+"px)";
-    console.log(this.mouseState.currentX);
+  handleOnRightBtn(){
+    console.log("handleOnRightBtn");
+    if(this.state.idx<this.slideState.maxSlide-1){
+      this.setState((prevState) => ({
+        idx : prevState.idx + 1
+      }));
+      this.slideState.idx++;
+      this.moveTimeline(-1);
+    } 
+  }
+  handleOnLeftBtn(){
+    console.log("handleOnLeftBtn");
+    if(this.state.idx>0){
+      this.setState((prevState) => ({
+        idx : prevState.idx - 1
+      }));
+      this.slideState.idx--;
+      this.moveTimeline(1);
+    }
+  }
+
+  moveTimeline(direction){
+    this.slideState.currentX += direction*this.itemDiv[0].offsetWidth;
+    this.focusItem(direction);
+    this.tlDiv.current.style.transform="translateX("+this.slideState.currentX+"px)";
+  }
+
+  focusItem(direction){
+    console.log(this.slideState.idx+"::"+this.slideState.maxSlide);
+    if (this.slideState.idx+direction>=0 && this.slideState.idx+direction < this.slideState.maxSlide) {
+      this.itemDiv[this.slideState.idx+direction].childNodes[0].setAttribute("style","none;");
+      this.itemDiv[this.slideState.idx+direction].childNodes[1].setAttribute("style","none;");
+    }
+
+    this.itemDiv[this.slideState.idx].childNodes[0].setAttribute("style","border:3px solid "+this.slideState.focusingColor);
+    this.itemDiv[this.slideState.idx].childNodes[1].setAttribute("style","border:3px solid "+this.slideState.focusingColor);
   }
 
   render(){
     const { contents } = this.state;
     const cList = contents.map(
-      (content) => (
-        <div className="item" >
+      (content,i) => (
+        <div className="item" ref={r => this.itemDiv[i]= r} key={i}> 
           <div className="mediaWrap">
             {content.mediaType==='image'?<img src={content.mediaSrc} alt={content.mediaSrc} />:<video src={content.mediaSrc} controls/>}
           </div>
-          <div className="textWrap">
+          <div className="contentsWrap">
             <span>{content.textSrc}</span>
           </div>
         </div >
       )
     );
     return (
-      <fragment>
+      <React.Fragment>
         <div className="outBox">
           <div className="innerBox" ref={this.contDiv}>
             <div className="cardAll noselect" ref={this.tlDiv}
-              onMouseDown ={e => this.handleMouseDown(e)}
+              // onMouseDown ={e => this.handleMouseDown(e)}
+              // onMouseMove={e => this.handleMouseMove(e)}
+              // onMouseUp={e => this.handleMouseUp(e)}
+              // onMouseOut={e => this.handleMouseOut(e)}
               onMouseMove={e => this.handleMouseMove(e)}
-              onMouseUp={e => this.handleMouseUp(e)}
-              onMouseOut={e => this.handleMouseOut(e)}
               >
               {cList}
             </div>
           </div>
         </div>
-        <div ckassName="navibtn">
-            <div className="navibtnLeft" onClick={this.handleOnLeftBtn}> &lt; </div>
-            <div className="navibtnRight" onClick={this.handleOnRightBtn}> &gt; </div>
+        <div className="navibtn">
+            <div className="navibtnLeft" onClick={this.handleOnLeftBtn}></div>
+            <span>{this.state.idx+1}/{this.slideState.maxSlide}</span>
+            <div className="navibtnRight" onClick={this.handleOnRightBtn}></div>
         </div>
-      </fragment>
+      </React.Fragment>
     );
   }
 };
